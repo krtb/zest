74c21f1d375e503c954f16a43eabde499a752a49
Kurt Bauer <kurtbauer55@protonmail.com>
2020-12-06 01:03:38 -0500

    display menu, order, and  inventory compoenents

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render(){
+        return (
+            <header className="top">
+                <h1>
+                    Zest
+                </h1>
+                <h3>
+                    <span>
+                        Fresh and Organic
+                    </span>
+                </h3>
+            </header>
+
+        )
+    }
+}
+
+export default Header;
